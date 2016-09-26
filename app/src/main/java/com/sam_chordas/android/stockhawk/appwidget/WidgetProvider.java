2d92cb188752095421b7c8deca21573148a52927
llin <dev.linxu@gmail.com>
2016-09-25 21:55:49 -0400

    Stock Hawk Stocks can be displayed in a collection widget.

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/appwidget/WidgetProvider.java
@@ -0,0 +1,37 @@
+package com.sam_chordas.android.stockhawk.appwidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        for (int i : appWidgetIds) {
+            updateWidget(context, appWidgetManager, i);
+        }
+    }
+
+    private void updateWidget(Context context, AppWidgetManager appWidgetManager,
+                      int appWidgetId) {
+        RemoteViews rv = new RemoteViews(context.getPackageName(),
+                R.layout.widget);
+        updateList(rv, context, appWidgetId);
+        appWidgetManager.updateAppWidget(appWidgetId, rv);
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId,
+                R.id.stock_list);
+    }
+
+    private void updateList(RemoteViews rv, Context context, int appWidgetId) {
+        Intent adapter = new Intent(context, WidgetService.class);
+        adapter.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        rv.setRemoteAdapter(R.id.stock_list, adapter);
+    }
+}
